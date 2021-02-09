95869da14edcf1db33e5a1e18c210a6ec2b29cdb
Hamza Butt <ibnetahir1@gmail.com>
2021-02-09 18:10:27 -0500

    Day 2 c# learning. - all the code I learned from day 2 self learning.

---
+++ branches-tutorial/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace BranchesAndLoops
+{
+    class Program
+    {
+        static void ExploreIf()
+        {
+            int a = 5;
+            int b = 3;
+            if (a + b > 10)
+            {
+                Console.WriteLine("The answer is greater than 10");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+            }
+
+            int c = 4;
+            if ((a + b + c > 10) && (a > b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("And the first number is greater than the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not greater than the second");
+            }
+
+            if ((a + b + c > 10) || (a > b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("Or the first number is greater than the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("And the first number is not greater than the second");
+            }
+        }
+
+        static void Loops()
+        {
+            for (int row = 1; row < 11; row++)
+            {
+                for (char column = 'a'; column < 'k'; column++)
+                {
+                    Console.WriteLine($"The cell is ({row}, {column})");
+                }
+            }
+        }
+
+        static void BLChallenge()
+        {
+            int answer = 0;
+            for(int index = 1; index <= 20; index++)
+            {
+                if((index % 3) == 0)
+                {
+                    answer += index;
+                }
+            }
+            Console.WriteLine($"The answer to the challenge is: {answer}");
+        }
+
+        static void Main(string[] args)
+        {
+            //ExploreIf();
+
+            //Loops();
+
+            BLChallenge();
+        }
+    }
+}
---
+++ numbers-quickstart/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace NumbersInCSharp
+{
+    class Program
+    {
+        static void WorkingWithIntegers()
+        {
+            int a = 18;
+            int b = 6;
+
+            // addition
+            int c = a + b;
+            Console.WriteLine(c);
+
+            // subtraction
+            c = a - b;
+            Console.WriteLine(c);
+
+            // multiplication
+            c = a * b;
+            Console.WriteLine(c);
+
+            // division
+            c = a / b;
+            Console.WriteLine(c);
+        }
+
+        static void OrderPrecedence()
+        {
+            int a = 5;
+            int b = 4;
+            int c = 2;
+            int d = a + b * c;
+            Console.WriteLine(d);
+
+            d = (a + b) * c;
+            Console.WriteLine(d);
+
+            d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+            Console.WriteLine(d);
+
+            int e = 7;
+            int f = 4;
+            int g = 3;
+            int h = (e  + f) / g;
+            Console.WriteLine(h);
+        }
+
+        static void TestLimits()
+        {
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
+
+            int what = max + 3;
+            Console.WriteLine($"An example of overflow: {what}");
+        }
+
+        static void WorkingWithDecimals()
+        {
+            decimal min = decimal.MinValue;
+            decimal max = decimal.MaxValue;
+            Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine(c / d);
+        }
+
+        static void AreaOfCircle()
+        {
+            double radius = 2.50;
+            double pi = Math.PI;
+            Console.WriteLine($"Area of Circle with radius 2.50 is: {(radius*radius)*pi}");
+        }
+
+        static void Main(string[] args)
+        {
+            // WorkingWithIntegers();
+
+            // OrderPrecedence();
+
+            // TestLimits();
+
+            // WorkingWithDecimals();
+
+            AreaOfCircle();
+        }
+    }
+}
